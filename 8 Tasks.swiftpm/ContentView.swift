213d7c56872e06fc9f9fcd9f3f71ed2e1613300c
Ferran Pujol Camins <ferranpujolcamins@gmail.com>
2024-08-23 15:53:09 +0200

    Bootstrap project

---
+++ 8 Tasks.swiftpm/ContentView.swift
@@ -0,0 +1,41 @@
+import SwiftUI
+
+struct Task: Identifiable, Hashable {
+    let id: UUID = UUID()
+    var title: String = ""
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(id)
+    }
+}
+
+struct TheState {
+    var tasks: [Task] = [Task(title: "A task")]
+}
+
+struct ContentView: View {
+    @State var state: TheState = TheState()
+    @FocusState private var focusedTask: Task?
+    var body: some View {
+        VStack {
+            ForEach($state.tasks) { $task in
+                TextField("", text: $task.title)
+                    .onSubmit {
+                        newTask(after: task)
+                    }
+                    .focused($focusedTask, equals: task)
+            }
+            Button("Add Task") {
+                newTask()
+            }
+        }
+    }
+    
+    private func newTask(after task: Task? = nil) {
+        let newTask = Task()
+        let index = task.map { t in state.tasks.firstIndex { $0.id == t.id}! }
+        ?? state.tasks.count - 1
+        state.tasks.insert(newTask, at: index + 1)
+        focusedTask = newTask
+    }
+}
